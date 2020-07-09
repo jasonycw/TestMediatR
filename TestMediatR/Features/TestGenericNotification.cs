01073ac430e6dd6dfb575e141b3bc2a2e4fddae0
Jason Yu <jasonycw1992@gmail.com>
2020-07-09 18:27:36 +0800

    Add another notification handler to proof generic INotification works

--- TestMediatR/Features/TestGenericNotification.cs
+++ TestMediatR/Features/TestGenericNotification.cs
@@ -29,4 +29,22 @@ namespace TestMediatR.Features
             _logger.LogInformation($"Notification value: {notification.Value.ToString()}");
         }
     }
+
+    public class AnotherTestGenericNotificationHandler<T> : INotificationHandler<TestGenericNotification<T>>
+    {
+        private readonly ILogger<AnotherTestGenericNotificationHandler<T>> _logger;
+
+        public AnotherTestGenericNotificationHandler(ILogger<AnotherTestGenericNotificationHandler<T>> logger)
+            => _logger = logger;
+
+        public async Task Handle(TestGenericNotification<T> notification, CancellationToken cancellationToken)
+        {
+            if (notification is null)
+                throw new ArgumentNullException(nameof(notification));
+            if (notification.Value is null)
+                throw new ArgumentNullException(nameof(notification.Value));
+
+            _logger.LogInformation($"Notification value: {notification.Value.ToString()}");
+        }
+    }
 }
